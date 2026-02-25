e12cf9ba21015bccd30d781e39845fdb28b177cb
Andres Baena <andres.baena46@eia.edu.co>
2026-02-25 11:36:27 -0500

    Atributos Orden, buen dia :D

--- src/Main/Orden.java
+++ src/Main/Orden.java
@@ -1,6 +1,12 @@
 package Main;
 
-public class Orden {
+import java.util.UUID;
 
-    
+public class Orden {
+    private UUID idPedido;
+    private Cliente cliente;
+    private String fecha;
+    private Enum estado;
+    private Enum metodoPago;
+    private OrdenItem items[];
 }
