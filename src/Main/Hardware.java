a21d9d6b130cd09cdcccd9af1a736964e495ce4a
apnauj <jxxnpx@gmail.com>
2026-02-25 08:11:56 -0500

    Clases básicas creadas

---
+++ src/Main/Hardware.java
@@ -0,0 +1,11 @@
+package Main;
+
+public class Hardware extends Producto{
+
+    public Hardware(String nombre, String descripcion, String categoria, int tiempoGarantia, double precioBase,
+            int stock) {
+        super(nombre, descripcion, categoria, tiempoGarantia, precioBase, stock);
+    }
+
+    
+}
---
+++ src/Main/Videojuego.java
@@ -0,0 +1,10 @@
+package Main;
+
+public class Videojuego extends Producto{
+
+    public Videojuego(String nombre, String descripcion, String categoria, int tiempoGarantia, double precioBase,
+            int stock) {
+        super(nombre, descripcion, categoria, tiempoGarantia, precioBase, stock);
+    }
+    
+}
