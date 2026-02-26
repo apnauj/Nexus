8f0bfc0e8e85258928518e12df5d53c4cb1e7a4a
gomezasd <158174557+gomezasd@users.noreply.github.com>
2026-02-25 23:22:30 -0500

    Update Hardware.java

--- src/Main/Hardware.java
+++ src/Main/Hardware.java
@@ -1,11 +1,44 @@
 package Main;
 
-public class Hardware extends Producto{
+import java.util.Date;
 
-    public Hardware(String nombre, String descripcion, String categoria, int tiempoGarantia, double precioBase,
-            int stock) {
+
+public class Hardware extends Producto {
+
+    private float consumo;
+    private String fabricante;
+
+    public Hardware(String nombre,String descripcion,String categoria,int tiempoGarantia,double precioBase,int stock,float consumo,String fabricante) {
         super(nombre, descripcion, categoria, tiempoGarantia, precioBase, stock);
+
+        this.consumo = consumo;
+        this.fabricante = fabricante;
+    }
+    public float getConsumo() {
+        return consumo;
+    }
+    public String getFabricante() {
+        return fabricante;
+    }
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+    public void setConsumo(float consumo) {
+        this.consumo = consumo;
+    }
+    public void actualizarStock(int nuevoStock) {
+        setStock(nuevoStock);
+    }
+    public boolean verificarDisponibilidad() {
+        return stock > 0;
     }
 
-    
+    @Override
+    void asignarDescuento() {
+        if (consumo < 100) { //mas consumo, menor descuento
+            descuento = 0.12;  // 12%
+        } else {
+            descuento = 0.05;  // 5%
+        }
+    }
 }
